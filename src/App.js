3b32384c3a61ee375d5cd2c4f1a6b9f1e5c5353a
hunhunseunghun <hunhunseunghun@gmail.com>
2023-04-12 11:03:41 +0900

    google Oauth signin func doing~

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
-import { firebaseConfig1, firebaseDB , dbRef, writeUserData} from "./Firebase.jsx"
+
+import {useEffect,useState} from "react"
+import { firebaseDB , dbRef, writeUserData} from "./Firebase.jsx"
+// google Oauth
+import { getAuth, getRedirectResult, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
+
 
 function App() {
   console.log(process.env.REACT_APP_PROJECT_ID)
@@ -10,23 +15,49 @@ const onclickHandler = () => {
   console.log("onclick executed")
 }
 
+
+//google Oauth
+
+const [googleSignedUser, setGoogleSignedUser] = useState(null);
+
+const googleSigninHandler =  async () => {
+  const googleProvider =  new GoogleAuthProvider();
+  const googleAuth = getAuth()
+
+
+
+
+  // gives google access token. can use it to access the google API
+  signInWithPopup(googleAuth, googleProvider)
+  .then((result) => {
+    const credential = GoogleAuthProvider.credentialFromResult(result);
+    const token = credential.accessToken;
+    //signed-in user info
+    const user = result.user;
+    setGoogleSignedUser(user)
+   
+  }).catch((error) => {
+    // Handle Errors here.
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    // email of the user's account used.
+    const email = error.customDtat.email;
+    // The firebase.auth.AuthCredential type that was used.
+    const credential = GoogleAuthProvider.credentialFromError(error);
+    
+    console.log("google signin error", errorCode, errorMessage, email, credential)
+  })
+}
+
+    
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
       <button onClick={onclickHandler}>TEST crud DATABASE</button>
+      <div>welcome, {googleSignedUser}</div>
+      <button onclick={googleSigninHandler}>Signin with Google</button>
     </div>
   );
 }
